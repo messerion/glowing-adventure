fe8d1f1d1d1d44629596c518a8f661b2704513cc
messerion <irka1395@gmail.com>
2024-11-10 14:40:04 +0300

    module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,1 +1,18 @@
-def add_everything_up:
+def add_everything_up(a, b):
+    try:
+
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        else:
+
+            return str(a) + str(b)
+    except TypeError:
+
+        return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
