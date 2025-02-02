0d218d3b2783ef3c09be887d17880677a8754c81
messerion <irka1395@gmail.com>
2025-02-02 20:33:15 +0300

    tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -7,43 +7,43 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Инициализация словаря результатов"""
+
         cls.all_results = {}
 
     def setUp(self):
-        """Создание бегунов перед каждым тестом"""
+
         self.usain = Runner("Усэйн", 10)
         self.andrey = Runner("Андрей", 9)
         self.nick = Runner("Ник", 3)
 
     def test_usain_nick_race(self):
-        """Забег Усэйна и Ника"""
+
         tournament = Tournament(90, [self.usain, self.nick])
         results = tournament.start()
 
-        # Проверка, что Ник финиширует последним
+
         self.assertEqual(results[2], "Ник")
 
-        # Сохраняем результаты для вывода в tearDownClass
+
         self.__class__.all_results[1] = results
 
     def test_andrey_nick_race(self):
-        """Забег Андрея и Ника"""
+
         tournament = Tournament(90, [self.andrey, self.nick])
         results = tournament.start()
 
-        # Проверка, что Ник финиширует последним
+
         self.assertEqual(results[2], "Ник")
 
-        # Сохраняем результаты для вывода в tearDownClass
+
         self.__class__.all_results[2] = results
 
     def test_three_runners_race(self):
-        """Забег Усэйна, Андрея и Ника"""
+
         tournament = Tournament(90, [self.usain, self.andrey, self.nick])
         results = tournament.start()
 
-        # Проверка порядка финиша
+
         self.assertEqual(results[3], "Ник")
         self.assertEqual(results[1], "Усэйн")
 
@@ -52,7 +52,7 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Вывод результатов после всех тестов"""
+        
         print("\nРезультаты всех тестов:")
         for key, value in cls.all_results.items():
             print(f"Тест {key}: {value}")
